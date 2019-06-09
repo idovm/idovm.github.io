7f48024158341bc1ebc8cfb0cec519562f4df5bb
idov <idov.mamane@gmail.com>
2019-06-09 22:32:50 +0200

    Commit test 2

---
+++ precache-manifest.dbc3ffcb7d8ed20a8fd23a8b5e3ccc3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0235235e5caaedb9a36f845acd8efb12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33a74508a199130d1a94",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "765b52967b2e21a3c86c",
+    "url": "/static/js/2.8a3c1329.chunk.js"
+  },
+  {
+    "revision": "33a74508a199130d1a94",
+    "url": "/static/js/main.e19720d6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
